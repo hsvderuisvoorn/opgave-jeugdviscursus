a1ceb3c520a473563ab2aba4e360ac113e8df5d2
hsvderuisvoorn <secretariaat@hsvderuisvoorn.nl>
2026-09-21 19:27:45 +0200

    Startpagina: zin over deelname-bevestiging verwijderd (staat al op de bedanktpagina) + SW-cache v5

--- sw.js
+++ sw.js
@@ -6,12 +6,12 @@
    zodra de app voor het eerst geladen wordt. Bij elke wijziging
    van de bestanden deze CACHE-NAAM opvoeren (nieuwe versie).
 
-   CACHE-NAAM: v4-20260921
+   CACHE-NAAM: v5-20260921
    ============================================================ */
 
 "use strict";
 
-var CACHE_NAAM = "opgave-jeugdviscursus-v4";
+var CACHE_NAAM = "opgave-jeugdviscursus-v5";
 
 var PRECACHE = [
   "./",
