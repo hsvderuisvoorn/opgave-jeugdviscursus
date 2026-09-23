8d380e1baa4371d34492958f306170ce1df52305
hsvderuisvoorn <secretariaat@hsvderuisvoorn.nl>
2026-09-23 19:16:08 +0200

    Hoe-werkt-het: punt 3 verwijst nu vetgedrukt naar de bedankpagina + SW-cache v13

--- sw.js
+++ sw.js
@@ -6,12 +6,12 @@
    zodra de app voor het eerst geladen wordt. Bij elke wijziging
    van de bestanden deze CACHE-NAAM opvoeren (nieuwe versie).
 
-   CACHE-NAAM: v12-20260923
+   CACHE-NAAM: v13-20260923
    ============================================================ */
 
 "use strict";
 
-var CACHE_NAAM = "opgave-jeugdviscursus-v12";
+var CACHE_NAAM = "opgave-jeugdviscursus-v13";
 
 var PRECACHE = [
   "./",
