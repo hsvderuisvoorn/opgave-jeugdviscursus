3b1a289a71a017b84b8dd9181e4cf02698c70e02
hsvderuisvoorn <secretariaat@hsvderuisvoorn.nl>
2026-09-23 19:17:51 +0200

    Aanmeldformulier: intro verwijst naar de bedankpagina + SW-cache v14

--- sw.js
+++ sw.js
@@ -6,12 +6,12 @@
    zodra de app voor het eerst geladen wordt. Bij elke wijziging
    van de bestanden deze CACHE-NAAM opvoeren (nieuwe versie).
 
-   CACHE-NAAM: v13-20260923
+   CACHE-NAAM: v14-20260923
    ============================================================ */
 
 "use strict";
 
-var CACHE_NAAM = "opgave-jeugdviscursus-v13";
+var CACHE_NAAM = "opgave-jeugdviscursus-v14";
 
 var PRECACHE = [
   "./",
